35efa1bd3f1dc1e37acc3584a5f51009107be276
Michiel <33991635+devbym@users.noreply.github.com>
2019-12-10 01:00:24 +0100

    Added argumentparser and searchfunction

--- getCurrency_v0.3.py
+++ getCurrency_v0.3.py
@@ -1,13 +1,35 @@
 from bs4 import BeautifulSoup , element
 from requests import get
-
-target = get('https://www.ecb.europa.eu/stats/policy_and_exchange_rates/euro_reference_exchange_rates/html/index.en.html').text
-
-soup = BeautifulSoup(target, features="html.parser")
-table = soup.table.tbody
-
-currlist = [[x.td.attrs.get('id'), x.span.string] for x in table.find_all('tr') if isinstance(x, element.Tag)]
+import argparse
+import sys
 
 
-for x in currlist:
-    print(f' 1 EUR = {x[1]} {x[0]}')
+parser = argparse.ArgumentParser(prog='Euro XREF Currency Lookup Tool', epilog="\n\nSource: ECB European Central Bank Euro Exchange Referral - https://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml \n")
+parser.add_argument('search' , nargs='?', help='Type a currency shorthand to search', default=False)
+parser.add_argument("-l", "--list", help="Return list of currencies and exit", action="store_true", default=False)
+
+args = parser.parse_args()
+
+def get_list():
+    target = get('https://www.ecb.europa.eu/stats/policy_and_exchange_rates/euro_reference_exchange_rates/html/index.en.html').text
+    soup = BeautifulSoup(target, features="html.parser")
+    table = soup.table.tbody
+    return [[x.td.attrs.get('id'), x.span.string] for x in table.find_all('tr') if isinstance(x, element.Tag)]
+
+if len(sys.argv) <= 1:
+    parser.print_help()
+    sys.exit(1)
+
+if args.search:
+    c = args.search
+    for x in get_list():
+        if c in x[0]:
+            print(f' 1 EUR = {x[1]} {x[0]}')
+
+if args.list:
+    for x in get_list():
+        print(x[0])
+
+
+
+
